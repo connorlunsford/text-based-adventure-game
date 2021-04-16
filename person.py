52f6287fd4a7b9382a45a5a5c47063c6d53d4169
Jennifer Briere <JenniferBriere@gmail.com>
2021-04-16 13:41:39 -0700

    basic person class complete

--- person.py
+++ person.py
@@ -1,7 +1,27 @@
 class Person:
 
-    def __init__(self):
-        return
+    def __init__(self, id: str, desc: str):
+        # contains id for the person (name of the person)
+        self._id = id
+        # contains the description of the person
+        self._desc = desc
+    
+    # methods for managing ID
 
     def get_id(self):
-        return
+        # returns the value of self._id
+        return self._id
+
+    def set_id(self, id: str):
+        # takes a string and sets it as the value of self._id
+        return self._id        
+
+    # methods for managing desc
+
+    def get_desc(self):
+        # returns the value of self._desc
+        return self._desc
+
+    def set_desc(self, desc: str):
+        # takes a string and sets it as the value of _desc
+        self._desc = descgit 
