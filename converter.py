cbc1784eb74c1b89ff690b92b461c0bc003bb931
Connor Lunsford <lunsfoco@oregonstate.edu>
2021-04-20 14:25:53 -0700

    Created the system class and added a whole bunch of functionality for it

--- converter.py
+++ converter.py
@@ -8,38 +8,21 @@ import room
 class Converter:
 
     def __init__(self):
-        self._objects = []
-        self._features = []
-        self._people = []
+        return
 
     def room_to_json(self, room_obj: room.Room):
         """takes a room object and converts it to a json object"""
-        # a list to store the object ids
-        obj_list = []
-        # runs through list of objects and gets the ids
-        for obj in room_obj.get_objects():
-            obj_list.append(obj.get_id())
-        # a list to store the feat ids
-        feat_list = []
-        # runs through list of features and gets the ids
-        for feat in room_obj.get_features():
-            feat_list.append(feat.get_id())
-        # a list to store the person ids
-        people_list = []
-        # runs through list of people and gets the ids
-        for person in room_obj.get_people():
-            people_list.append(person.get_id())
         # stores everything in a dict
         room_dict = {
             'id': room_obj.get_id(),
             'desc': room_obj.get_desc(),
             'sdesc': room_obj.get_sdesc(),
             'visited': room_obj.get_visited(),
-            'objects': obj_list,
-            'features': feat_list,
-            'people': people_list,
+            'objects': room_obj.get_objects(),
+            'features': room_obj.get_features(),
+            'people': room_obj.get_people(),
             'connections': room_obj.get_connections(),
-            'verbs': room_obj.get_verbs()
+            'interactions': room_obj.get_interactions()
         }
         # converts the dict to json file and returns it
         return json.dumps(room_dict)
@@ -49,43 +32,23 @@ class Converter:
         # turns the json file into a dict
         room_dict = json.loads(json_room)
         # gets all the basic variables from the dict
-        id = room_dict.get('id')
-        desc = room_dict.get('desc')
-        sdesc = room_dict.get('sdesc')
-        visited = room_dict.get('visited')
+        id = room_dict['id']
+        desc = room_dict['desc']
+        sdesc = room_dict['sdesc']
+        visited = room_dict['visited']
         # creates a new room with the variables
         new_room = room.Room(id, desc, sdesc, visited)
-
-        # creates an object list
-        obj_ids = room_dict.get('objects')
-        # runs through the database of objects
-        for obj in self._objects:
-            # if it finds an object that matches the id given
-            if obj.get_id() in obj_ids:
-                # removes the id from the id list
-                obj_ids.remove(obj.get_id)
-                # adds the object to the room
-                new_room.add_object(obj)
-
-        # adds all features, same as objects above
-        feat_ids = room_dict.get('features')
-        for feat in self._features:
-            if feat.get_id() in feat_ids:
-                feat_ids.remove(feat.get_id)
-                new_room.add_feature(feat)
-
-        # adds all people, same as objects above
-        person_ids = room_dict.get('people')
-        for person in self._people:
-            if person.get_id() in person_ids:
-                person_ids.remove(person.get_id)
-                new_room.add_person(person)
-
+        # adds all the objects into the room
+        new_room.set_objects = room_dict['objects']
+        # adds all the features into the room
+        new_room.set_features = room_dict['features']
+        # adds all the people into the room
+        new_room.set_people = room_dict['people']
         # adds all the room ids in 'connections' into self._connections
-        new_room.set_connections(room_dict.get('connections'))
+        new_room.set_connections(room_dict['connections'])
 
         # adds the verbs from 'verbs' into self._verbs
-        new_room.set_verbs(room_dict.get('verbs'))
+        new_room.set_interactions(room_dict['interactions'])
 
         # returns the new_room object as a Room object
         return new_room
@@ -97,7 +60,7 @@ class Converter:
             'id': object_obj.get_id(),
             'name': object_obj.get_named()
         }
-        
+
         # converts the dict to a json file and returns it
         return json.dumps(object_dict)
 
@@ -123,7 +86,7 @@ class Converter:
             'name': feat_obj.get_name(),
             'interactions': feat_obj.get_interactions()
         }
-        
+
         # converts the dict to an json file and returns it
         return json.dumps(feature_dict)
 
@@ -135,10 +98,10 @@ class Converter:
         id = feature_dict.get('id')
         name = feature_dict.get('name')
         interactions = feature_dict.get('interactions')
-        
+
         # creates a new Feature object with the specified attributes
         new_feature = feature.Feature(id, name, interactions)
-        
+
         # returns the new_feature object as a Feature object
         return new_feature
 
