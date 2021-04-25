a2a4ffb1c3963733b34a2941b4f4ab31e60fd00d
Jennifer Briere <JenniferBriere@gmail.com>
2021-04-25 14:22:47 -0700

    unit tests for Person class

--- person.py
+++ person.py
@@ -2,5 +2,5 @@ from interactable import Interactable
 
 class Person(Interactable):
 
-    def __init__(self, id: str, name: str, desc: str, interactions):
+    def __init__(self, id: str, name: str, desc: str, interactions: dict):
         super().__init__(id, name, desc, interactions)
---
+++ test_person.py
@@ -0,0 +1,38 @@
+import unittest
+import person
+ 
+class TestPersonClass(unittest.TestCase):
+
+    def setUp(self):
+        id = "01"
+        name = "Ralph"
+        desc = "Ralph has been the groundskeeper for many years."
+        interactions = {"look at": """The groundskeeper is acting nervous
+            and seems to have misplaced something."""} 
+        self.test_person = person.Person(id, name, desc, interactions)
+
+    def test_get_id(self):
+        expected = "01"
+        self.assertEqual(self.test_person.get_id(), expected)
+
+    def test_get_name(self):
+        expected = "Ralph"
+        self.assertEqual(self.test_person.get_name(), expected)
+
+    def test_set_name(self):
+        new_name = "Bob"
+        self.test_person.set_name(new_name)
+        self.assertEqual(self.test_person.get_name(), new_name)
+
+    def test_get_desc(self):
+        """by default also test set_desc"""
+        new_desc = """Ralph has been the groundskeeper for 
+            many years. He loves his job but feels underappreciated by
+            his boss."""
+        self.test_person.set_desc(new_desc)
+        self.assertEqual(self.test_person.get_desc(), new_desc)        
+
+    """I realize now that Person doesn't currently have anything above and
+    beyond the Interactable class attributes and there is already unit tests
+    for all of those methods in test_interactable. I'm not sure that a
+    separate Person test suite is even necessary at this point in time."""
